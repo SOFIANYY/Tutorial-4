165b1f9528ea34e0954571b7bfa633c2e87727cb
SOFIAN YAHYAOUI <sofian.yahyaoui4@gmail.com>
2023-04-05 15:13:02 +0200

    second  commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+/*import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,3 +20,4 @@ const router = createRouter({
 })
 
 export default router
+*/
